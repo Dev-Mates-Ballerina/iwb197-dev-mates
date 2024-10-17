b24c95f7874be5837aed2a96fa508e21d567c3b2
ChristinaRandeniya <christinarandeniya123@gmail.com>
2024-10-17 18:05:08 +0530

    UpdatedEvents

--- frontend/src/pages/NewEvent/Category.js
+++ frontend/src/pages/NewEvent/Category.js
@@ -1,10 +1,87 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import '../../css/Category.css'
+import { Link } from 'react-router-dom';
+import dollar from '../../images/dollar.png';
+import cultural from '../../images/cultural.png'
+import tech from '../../images/tech.png'
+import cooperate from '../../images/cooperate.png'
+import other from '../../images/other.png'
+import proms from '../../images/proms.png'
+import party from '../../images/party.png'
+import wedding from '../../images/wedding.png'
+import engagement from '../../images/engagement.png'
 
 function Category() {
   return (
     <div>
       <Navbar/>
+      <div className='main-content'>
+          <div className='category-selection-sidebar'>
+            <div class="sidebar1">
+              <ol class="steps">
+                  <li class="step current"><Link to='/newEvent'>1</Link></li>
+                  <li class="step"><Link to='/newEvent/Category'>2</Link></li>
+                  <li class="step"><Link to='/newEvent/ResourcePerson'>3</Link></li>
+                  <li class="step"><Link to='/newEvent/Charity'>4</Link></li>
+                  <li class="step"><Link to='/newEvent/Budget'>5</Link></li>
+              </ol>
+            </div>
+          </div>
+          <div className='category-selection-rightside'>
+            <div className='category-selection-rightside-header'>
+              <div>
+                <h2 className='category-selection-rightside-headerH2'>Step 2</h2>
+              </div>
+              <div className="budget">
+                <img src={dollar} alt="" width='50px'/>
+                <h3>LKR 0</h3>
+              </div>
+            </div>
+            <div className='category-selection-rightside-body'>
+            <div>
+              <p class="category-selection-rightside-body-descriptionP1">What kind of event are you planning?</p>
+              <p className='category-selection-rightside-body-descriptionP1'>Cultural events, Tech events, corporate events, prom, parties, weddings, engagement parties, and others. Please choose from the options so we can help you plan your events.</p>
+            </div>
+            <div class="category-selection-categories">
+                <a href="/new-event/photographers"><div class="category-item">
+                    <img src={cultural} alt="Photographers" />
+                    <p>Cultural events</p>
+                </div></a>
+                <div class="category-item">
+                    <img src={tech} alt="Entertainment" />
+                    <p>Tech events</p>
+                </div>
+                <div class="category-item">
+                    <img src={cooperate} alt="Catering" />
+                    <p>Co-operate events</p>
+                </div>
+                <div class="category-item">
+                    <img src={proms} alt="Locations" />
+                    <p>Proms</p>
+                </div>
+                <div class="category-item">
+                    <img src={party} alt="Vehicles" />
+                    <p>Parties</p>
+                </div>
+                <div class="category-item">
+                    <img src={wedding} alt="Decorations" />
+                    <p>Weddings</p>
+                </div>
+                <div class="category-item">
+                    <img src={engagement} alt="Electronics" />
+                    <p>Engagements</p>
+                </div>
+                <div class="category-item">
+                    <img src={other} alt="Other" />
+                    <p>Other</p>
+                </div>
+            </div>
+            <button class="next-button">Next</button>
+          </div>
+          </div>
+
+      </div>
     </div>
   )
 }
--- frontend/src/pages/NewEvent/CreateEvent.js
+++ frontend/src/pages/NewEvent/CreateEvent.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import '../../css/CreateEvent.css'
+import { Link } from'react-router-dom';
+
 
 function CreateEvent() {
   return (
     <div>
       <Navbar/>
         <div className='main-content'>
-            <h1>event</h1>
+            <div className='create-event-H'>
+              <h1 className='create-eventH1'>Create event</h1>
+            </div>
+            <div className='create-event-sidebar-and-form'>
+              <div className='create-event-sidebar-div'>
+                <div class="sidebar1">
+                  <ol class="steps">
+                      <li class="step current"><Link to='/newEvent'>1</Link></li>
+                      <li class="step"><Link to='/newEvent/Category'>2</Link></li>
+                      <li class="step"><Link to='/newEvent/ResourcePerson'>3</Link></li>
+                      <li class="step"><Link to='/newEvent/Charity'>4</Link></li>
+                      <li class="step"><Link to='/newEvent/Budget'>5</Link></li>
+                  </ol>
+                </div>
+              </div>
+              <div className='create-event-form-div'>
+                <h2 className='step1H2'>Step 1</h2>
+                <p className='create-new-eventP'>Create a new event</p>
+                  <div className='create-event-form-input'>
+                    <input className='create-event-name-input' type='text' id='event-name' name='event-name' required placeholder='Event name' />
+                  </div>
+                  <div className='create-event-form-input'>
+                    <input className='create-event-date-input' type='date' id='date' name='date' required placeholder='Event date'/>
+                  </div>
+                  <div className='create-event-form-input'>
+                    <input className='create-event-budget-input' type='text' id='budget' name='budget' required placeholder='Estimated budget'/>
+                  </div>
+                  <div className='create-event-form-buttonDiv'>
+                    <button className='create-event-button'>Create</button>
+                  </div>
+                  <div className='create-event-form-buttonDiv'>
+                    <button className='create-event-back-button'>Back</button>
+                  </div>
+              </div>
+            </div>
         </div>
     </div>
   )
