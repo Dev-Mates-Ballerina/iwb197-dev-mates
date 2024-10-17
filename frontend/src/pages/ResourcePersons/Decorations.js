bbe50040e80cb5a161687e5b6b313c0ba7b3930f
H.M.D.B. Bandara <130538167+dshnz29@users.noreply.github.com>
2024-10-18 04:53:03 +0530

    //

--- frontend/src/pages/ResourcePersons/Decorations.js
+++ frontend/src/pages/ResourcePersons/Decorations.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from '../../components/Navbar'
 import '../../css/Decorations.css'
 import { Link } from'react-router-dom'
@@ -9,8 +9,60 @@ import deco4 from '../../images/deco4.jpg';
 import deco5 from '../../images/deco5.jpg';
 import deco6 from '../../images/deco6.jpg';
 import dollar from '../../images/dollar.png'
+import { DataContext } from '../../context/DataProvider';
 
 function Decorations() {
+    const { addCard } = useContext(DataContext);
+
+    const cardsData = [
+        {
+            name: "Elite Event Creators",
+            contact: "+94 71 111 1111",
+            email: "info@elitecreators.com",
+            price: "LKR 162,500",
+            imgSrc: deco1
+        },
+        {
+            name: "Sparkle Celebrations",
+            contact: "+94 71 111 1111",
+            email: "contact@sparkle.com",
+            price: "LKR 146,250",
+            imgSrc: deco2
+        },
+        {
+            name: "Dream Day Planners",
+            contact: "+94 71 111 1111",
+            email: "hello@dreamday.com",
+            price: "LKR 195,000",
+            imgSrc: deco3
+        },
+        {
+            name: "Glamour Event Solutions",
+            contact: "+94 71 111 1111",
+            email: "support@glamourevents.com",
+            price: "LKR 178,750",
+            imgSrc: deco4
+        },
+        {
+            name: "Festive Moments Co.",
+            contact: "+94 71 111 1111",
+            email: "inquiries@festive.com",
+            price: "LKR 130,000",
+            imgSrc: deco5
+        },
+        {
+            name: "Perfect Planner",
+            contact: "+94 71 111 1111",
+            email: "info@perfectlyplanner.com",
+            price: "LKR 211,250",
+            imgSrc: deco6
+        }
+    ];
+
+    const handleSubmit = (decoration) => {
+        addCard(decoration);
+    };
+
   return (
     <div>
       <Navbar/>
@@ -39,19 +91,23 @@ function Decorations() {
               <div class="decoration">
                 <h4 class="decoration-descriptionH4">Select the Decoration team you want</h4>
                 <div class="decorationTeams">
-                  <div class="decoration1">
-                    <div>
-                      <img src={deco1} />
+
+                  {cardsData.map((decoration, index) => (
+                    <div className="decoration1" key={index}>
+                        <div>
+                            <img src={decoration.imgSrc} alt={decoration.name} />
+                        </div>
+                        <div className="decorationText">
+                            <p>{decoration.name}</p>
+                            <p>{decoration.contact}</p>
+                            <p>{decoration.email}</p>
+                            <p>{decoration.price}</p>
+                        </div>
+                        <button className="decoration-choose" onClick={() => handleSubmit(decoration)}>Choose</button>
                     </div>
-                    <div class="decorationText">
-                      <p1>Elite Event Creators</p1>
-                      <p>+94 71 111 1111</p>
-                      <p>info@elitecreators.com</p>
-                      <p>LKR 162,500</p>
-                    </div>
-                    <button class="decoration-choose">Choose</button>
-                  </div>
-                  <div class="decoration2">
+                ))}
+
+                  {/* <div class="decoration2">
                     <div>
                       <img src={deco2} />
                     </div>
@@ -74,9 +130,9 @@ function Decorations() {
                       <p>LKR 195,000</p>
                     </div> 
                     <button class="decoration-choose">Choose</button> 
-                  </div>
+                  </div> */}
                 </div>
-                <div class="decorationTeams1">
+                {/* <div class="decorationTeams1">
                   <div class="decoration4">
                     <div>
                       <img src={deco4} />
@@ -113,7 +169,7 @@ function Decorations() {
                     </div>  
                     <button class="decoration-choose">Choose</button>
                   </div>
-                </div>
+                </div> */}
               </div>  
             </div>
               <button class="decoration-back-button">Back</button>
